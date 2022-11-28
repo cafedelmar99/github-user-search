d6af718ced3f4c496c27cddb2f22bad4ff847eb0
cafedelmar99 <zukachakhnashvili@gmail.com>
2022-11-28 20:35:27 +0400

    finalized

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,49 +1,64 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import search from '../assets/icon-search.svg'
+import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+import search from "../assets/icon-search.svg";
 
 function Input(props) {
   const [input, setInput] = useState("");
-  const handleChange = event => {
+  const handleChange = (event) => {
     setInput(event.target.value);
-  }
-
-  const handleClick = () => {
-    axios({
-      method: 'get',
-      url: `https://api.github.com/users/${input}`,
-    })
-      .then(function (response){
-        props.setInfo(response.data);
-        console.log(response.data);
-      })
-  }
+  };
+  const [error, setError] = useState(false);
+  const handleClick = async () => {
+    try {
+      const { data } = await axios?.get(
+        `https://api.github.com/users/${input}`
+      );
+      props.setInfo(data);
+      setError(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
     <InputHolder modeStatus={props.modeStatus}>
-      <img src={search}/>
-      <Search onChange={handleChange} modeStatus={props.modeStatus} type="text" placeholder='Search Github username'/>
-      <SearchButton onClick={handleClick} modeStatus={props.modeStatus}>Search</SearchButton>
+      <>
+      <img src={search} />
+      <Search
+        onChange={handleChange}
+        modeStatus={props.modeStatus}
+        type="text"
+        placeholder="Search Github username"
+      />
+      </>
+      {error ? <p>No results</p> : null}
+      <SearchButton onClick={handleClick} modeStatus={props.modeStatus}>
+        Search
+      </SearchButton>
     </InputHolder>
-  )
+  );
 }
 const InputHolder = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   margin-top: 35px;
-  width: 100%;
   background-color: ${(props) => (props.modeStatus ? "#1E2A47" : "#FFF")};
   border-radius: 10px;
-  &>img{
+  & > img {
     width: 20px;
     height: 20px;
     margin-left: 16px;
   }
-  @media (min-width: 768px){
-   justify-content: space-between;
-   max-width: 573px;
-   margin: 35px auto 0px auto;
+  &>p{
+    color: red;
+    font-size: 15px;
+  }
+  @media (min-width: 768px) {
+    justify-content: space-between;
+    max-width: 573px;
+    margin: 35px auto 0px auto;
   }
 `;
 
@@ -52,17 +67,18 @@ const Search = styled.input`
   font-size: 13px;
   font-weight: 400;
   border: none;
+  max-width:40%;
   background-color: ${(props) => (props.modeStatus ? "#1E2A47" : "#FFF")};
   color: ${(props) => (props.modeStatus ? "white" : "#4B6A9B")};
-  ::placeholder{
+  ::placeholder {
     text-align: center;
     color: ${(props) => (props.modeStatus ? "white" : "#4B6A9B")};
     font-size: 13px;
   }
-  @media (min-width: 768px){
-   width: 100%;
-   margin-left: 20px;
-   ::placeholder{
+  @media (min-width: 768px) {
+    width: 100%;
+    margin-left: 20px;
+    ::placeholder {
       text-align: start;
     }
   }
@@ -71,21 +87,21 @@ const Search = styled.input`
 const SearchButton = styled.button`
   width: 84px;
   height: 50px;
-  background-color: #0079FF;
+  background-color: #0079ff;
   border-radius: 10px;
   border: none;
   margin-right: 7px;
   font-size: 14px;
   font-weight: 700;
-  color: #FFF;
-  :hover{
-    background-color: #60ABFF;
+  color: #fff;
+  :hover {
+    background-color: #60abff;
   }
-  @media (min-width: 768px){
-   width: 106px;
-   margin-left: 20px;
-   font-size: 16px;
+  @media (min-width: 768px) {
+    width: 106px;
+    margin-left: 20px;
+    font-size: 16px;
   }
 `;
 
-export default Input
+export default Input;
